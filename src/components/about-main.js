1fc93b0bfa66f0a1736132769eee88d2174b76fb
Brian Pham <brianlpham21@gmail.com>
2018-03-23 00:39:55 -0700

    responsive design for front page

--- src/components/about-main.js
+++ src/components/about-main.js
@@ -5,11 +5,13 @@ import './about-main.css';
 
 export default function AboutMain() {
   return (
-    <div className='main-about'>
-      <Link to='/about' className='main-about-link-component'>
-        <button className='about-button'><h2 className='main-about-link'>About Training Tracker</h2></button>
-      </Link>
-      <p>The Training Tracker application aims to make it easier for those looking to track their fitness progress</p>
+    <div className='main-about-container'>
+      <div className='main-about'>
+        <Link to='/about' className='main-about-link-component'>
+          <button className='about-button'><h2 className='main-about-link'>About Training Tracker</h2></button>
+        </Link>
+        <p>The Training Tracker application aims to make it easier for those looking to track their fitness progress</p>
+      </div>
     </div>
   )
 }
--- src/components/instructions-main.js
+++ src/components/instructions-main.js
@@ -4,10 +4,12 @@ import './instructions-main.css'
 
 export default function InstructionsMain() {
   return (
-    <div className='main-instructions'>
-      <img src='https://www.birdiebox.com/images/custom-icon.gif?crc=381262841' alt='instruction-icon' className='instruction-icon'/>
-      <h2>Customizable Lifting Workouts</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.</p>
+    <div className='main-instructions-container'>
+      <div className='main-instructions'>
+        <img src='https://www.birdiebox.com/images/custom-icon.gif?crc=381262841' alt='instruction-icon' className='instruction-icon'/>
+        <h2>Customizable Lifting Workouts</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.</p>
+      </div>
     </div>
   )
 }
