3fa37d2df3f5e99956f6916fa968896e5fc03423
Brian Pham <brianlpham21@gmail.com>
2018-03-23 14:54:14 -0700

    responsive design of home and logout buttons for dashboard

--- src/components/header-bar.js
+++ src/components/header-bar.js
@@ -16,7 +16,7 @@ export class HeaderBar extends React.Component {
     let logOutButton;
     if (this.props.loggedIn) {
       logOutButton = (
-        <div className='user-title-bar'>
+        <div className='header-title-bar'>
           <Link to='/'><button className='home-button'>Home</button></Link>
           <Link to='/' className='header-bar-link-component'><h2 className='header-bar-title'>Training Tracker</h2></Link>
           <button onClick={() => this.logOut()} className='logout-button'>Log out</button>
