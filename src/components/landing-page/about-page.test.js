98b602e07168b241b04aa3930aa3fdf07bc928b5
Brian Pham <brianlpham21@gmail.com>
2018-04-10 20:19:18 -0700

    empty commit

--- src/components/landing-page/about-page.test.js
+++ src/components/landing-page/about-page.test.js
@@ -2,9 +2,14 @@ import React from 'react';
 import {shallow} from 'enzyme';
 
 import AboutPage from './about-page';
+import TitleBar from './title-bar';
 
 describe('<AboutPage/>', () => {
   it('Renders without crashing', () => {
     shallow(<AboutPage />);
+  });
+
+  it('Renders title bar without crashing', () => {
+    shallow(<TitleBar />);
   });
 })
