23070d65ef05a557c6fd686e6c7e46867a48fda6
Brian Pham <brianlpham21@gmail.com>
2018-04-15 11:34:17 -0700

    tests for workouts, exercises, and sets reducers

---
+++ src/reducers/exercises.tests.js
@@ -0,0 +1,18 @@
+import reducer from './exercises';
+
+describe('workoutsReducer', () => {
+  it('Should set the initial state when nothing is passed in', () => {
+    const state = reducer(undefined, {type: '__UNKNOWN'});
+    expect(state).toEqual({
+      addExerciseData: '',
+      editExerciseData: '',
+      error: null
+    });
+  });
+
+  it('Should return the current state on an unknown action', () => {
+    let currentState = {};
+    const state = reducer(currentState, {type: '__UNKNOWN'});
+    expect(state).toBe(currentState);
+  });
+})
---
+++ src/reducers/workouts.test.js
@@ -0,0 +1,19 @@
+import reducer from './workouts';
+import {fetchWorkoutDataSuccess, addWorkoutSuccess} from '../actions/workouts';
+
+describe('workoutsReducer', () => {
+  it('Should set the initial state when nothing is passed in', () => {
+    const state = reducer(undefined, {type: '__UNKNOWN'});
+    expect(state).toEqual({
+      data: '',
+      addWorkoutData: '',
+      error: null
+    });
+  });
+
+  it('Should return the current state on an unknown action', () => {
+    let currentState = {};
+    const state = reducer(currentState, {type: '__UNKNOWN'});
+    expect(state).toBe(currentState);
+  });
+})
