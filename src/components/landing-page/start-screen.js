d46b5fc404014a71ff60fd9b6f6de9c1114612a8
Brian Pham <brianlpham21@gmail.com>
2018-07-17 11:35:48 -0700

    added functionality to view demo button

--- src/components/landing-page/start-screen.js
+++ src/components/landing-page/start-screen.js
@@ -1,45 +1,58 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import {Field, reduxForm, focus} from 'redux-form';
+
+import {login} from '../../actions/auth';
 
 import logo from '../../images/logo.png';
 import arrow from '../../images/arrow.png';
 
 import './start-screen.css';
 
-export default function StartScreen() {
-  return (
-    <div className='start-screen'>
-      <div className='container'>
-        <img
-          src={logo}
-          alt='main-logo'
-          className='main-logo'
-        />
-        <h2 className='title'>Training Tracker</h2>
-        <p className='title-description'>Set and Track Your Fitness Goals</p>
-        <ul>
-          <li>
-            <Link to='/register'>
-              <button className='register-button'>Register</button>
-            </Link>
-          </li>
-          <li>
-            <Link to='/login'>
-              <button className='login-button'>Log In</button>
-            </Link>
-          </li>
-          <li className="demo-button-link">
-            <Link to='/register'>
-              <button className='demo-button'>View Demo</button>
-            </Link>
-          </li>
-        </ul>
-        <img
-          src={arrow}
-          alt='arrow-down'
-          className='arrow-down'
-        />
+export class StartScreen extends React.Component {
+  onClick() {
+    const {username, password} = {username: 'demo', password: 'brianbrian'};
+    return this.props.dispatch(login(username, password))
+  }
+
+  render() {
+    return (
+      <div className='start-screen'>
+        <div className='container'>
+          <img
+            src={logo}
+            alt='main-logo'
+            className='main-logo'
+          />
+          <h2 className='title'>Training Tracker</h2>
+          <p className='title-description'>Set and Track Your Fitness Goals</p>
+          <ul>
+            <li>
+              <Link to='/register'>
+                <button className='register-button'>Register</button>
+              </Link>
+            </li>
+            <li>
+              <Link to='/login'>
+                <button className='login-button'>Log In</button>
+              </Link>
+            </li>
+            <li className="demo-button-link">
+              <button className='demo-button' onClick={() => this.onClick()}>View Demo</button>
+            </li>
+          </ul>
+          <img
+            src={arrow}
+            alt='arrow-down'
+            className='arrow-down'
+          />
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+export default reduxForm({
+  form: 'login',
+  onSubmitFail: (errors, dispatch) => dispatch(focus('login', 'username'))
+})(StartScreen);
