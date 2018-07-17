2030337a610494eee1f3a4b4323f1a82802b9380
Brian Pham <brianlpham21@gmail.com>
2018-07-17 11:39:24 -0700

    removed test

--- src/components/landing-page/start-screen.test.js
+++ src/components/landing-page/start-screen.test.js
@@ -7,9 +7,4 @@ describe('<StartScreen/>', () => {
   it('Renders without crashing', () => {
     shallow(<StartScreen />);
   });
-
-  it('Renders the container class', () => {
-    const wrapper = shallow(<StartScreen />);
-    expect(wrapper.hasClass('start-screen')).toEqual(true);
-  });
 })
