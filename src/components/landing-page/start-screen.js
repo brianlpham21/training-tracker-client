7e001b30eb8311998248552195d56dcc1eccd147
Brian Pham <brianlpham21@gmail.com>
2018-07-17 11:42:34 -0700

    rmeoved unused variable

--- src/components/landing-page/start-screen.js
+++ src/components/landing-page/start-screen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Field, reduxForm, focus} from 'redux-form';
+import {reduxForm, focus} from 'redux-form';
 
 import {login} from '../../actions/auth';
 
